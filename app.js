d29002f2d2f21a074c94d584dea73684d72e4690
evan012-bev <ebowser1@luc.edu>
2024-12-03 01:44:01 -0600

    Super basic outline of Financial planner

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addBudgetBtn = document.getElementById('addBudget');
+    addBudgetBtn.addEventListener('click', () => {
+        alert('Budgeting feature coming soon!');
+    });
+});
