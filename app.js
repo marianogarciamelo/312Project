649b4c6b459dbcfd7dce290c1e587a88d6070f0e
Jsatchell03 <jsatchell2017@gmail.com>
2024-12-04 01:15:12 -0600

    Cleaning up file struct

--- app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const addBudgetBtn = document.getElementById('addBudget');
-    addBudgetBtn.addEventListener('click', () => {
-        alert('Budgeting feature coming soon!');
-    });
-});
